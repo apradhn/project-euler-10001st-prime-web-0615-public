8baa52f39fb975e37ef5613f2453dad7ddc1e67a
apradhn <abhijit.pradhan6@gmail.com>
2015-07-10 15:03:56 -0400

    all done

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+def prime_number_for(nth_element)
+  i = 1
+  prime_count = 0
+  loop do 
+    i += 1
+    if prime?(i)
+      prime = i
+      prime_count += 1
+    end
+    # binding.pry
+    return prime if prime_count == nth_element
+  end
+end
+
+def prime?(num)
+  (2..(Math.sqrt(num).round)).none?{ |n| num % n == 0 }
+end
